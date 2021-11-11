59ab06a81e9a3e4abbc7a8a5b1f33dfc753ebae8
孟陞 彭 <108111108@mail.aeust.edu.tw>
2021-11-11 14:40:43 +0800

    練習6-1

---
+++ 110-1Practice6-1/RadioBut.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice6_1
+{
+    public partial class RadioBut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Form.Get("gender") == "male")
+            {
+                Response.Write("男");
+            }
+            else
+            {
+                Response.Write("女");
+            }
+
+        }
+    }
+}
